971dce9acdbff94d4a9716d3f31d152f219d959b
Milton Volpato <volpato.milton@principal.com>
2024-01-29 12:13:15 -0300

    20240129 Aula Final Criando Aplicacion Python

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 # standard and best practices: Snake Case
 import os
 
-restaurantes = ['Pizza', 'Sushi']
+restaurantes = [{'nome':'Praca', 'categoria':'Japonesa', 'ativo':False},
+                {'nome':'Pizza Suprema', 'categoria':'Pizza', 'ativo':True},
+                {'nome':'Cantina', 'categoria':'Italiano', 'ativo':False}]
+
+# Para contar a frequência de cada palavra em uma frase, você pode utilizar o seguinte código:
+#frase = "Python se tornou uma das linguagens de programação mais populares do mundo nos últimos anos."
+#contagem_palavras = {}
+#palavras = frase.split()
+#for palavra in palavras:
+#    contagem_palavras[palavra] = contagem_palavras.get(palavra, 0) + 1
+#print(contagem_palavras)
+
 
 def exibir_nome_do_programa():
     print("""
@@ -16,15 +27,15 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar Restaurantes')
-    print('3. Ativar Restaurantes')
+    print('3. Alternar Estado dos Restaurantes')
     print('4. Sair\n')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para retornar ao menu principal ')
+    main()
 
 def finalizar_app():
     exibir_subtitulo('Encerrando o Programa')
-
-def voltar_ao_menu_principal():
-    input('Digite uma tecla para retornar ao menu principal ')
-    main()
 
 def opcao_invalida():
     print('Opcao Invalida\n')
@@ -32,28 +43,60 @@ def opcao_invalida():
 
 def exibir_subtitulo(texto):
     os.system('clear')
+    linha = '*' * (len(texto) + 4) 
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
 def cadastrar_novo_restaurante():
+    '''Essa funcao e responsavel por cadastrar um novo Restaurante
+
+    Inputs:
+    - Nome do Restaurante
+    - Categoria
+
+    Outputs
+    - Adiciona um novo restaurante a lista de restaurantes
+    
+    '''
     exibir_subtitulo('Cadastro de Nuevos Restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que desejascadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite  o nome da categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
+    restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
     exibir_subtitulo('Listando Restaurantes')
 
+    print(f'{'Nome do Restaurante'.ljust(23)} | {'Categoria'.ljust(20)} | {'Status'}')
     for restaurante in restaurantes:
-        print(f'.{restaurante}')
-
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f' - {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
     #numero_tabuada = int(input("Digite um número para a tabuada: "))
     #for i in range(1, 11):
     #resultado = numero_tabuada * i
-    #print(f"{numero_tabuada} x {i} = {resultado}")
+    #print(f"{numero_tabuada} x {i} = {resultado}"
+    voltar_ao_menu_principal()
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alternando Estado do Restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+    restaurante_econtrado = False
 
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_econtrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            # Estrutura ternaria
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+    if not restaurante_econtrado:
+        print('O restaurante nao foi encontrado')
     voltar_ao_menu_principal()
 
 def escolher_opcao():
@@ -74,7 +117,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar Restaurantes')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
